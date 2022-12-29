Fixed few bugs, updated scaling to pyx SLHL

src/Pmag_paleolat.py
@@ -33,12 +33,17 @@ for i in range(0,len(Read.lat)):
         vals.append(pmag.apwp(data))
 
 #save outputs in usable dataframes for later.
+pl = np.repeat(Read.site_lat.values, len(time))
+pl_df = pd.DataFrame([(pl[n:n+len(time)]) for n in range(0, len(pl), len(time))])
 
-df = pd.DataFrame(vals) 
-df.columns = ['Age', 'Paleolat', 'Dec','Inc','Pole_lat','Pole_Long'] 
-Paleolat = df['Paleolat']
-pl_df = pd.DataFrame([(Paleolat.values.tolist()[n:n+len(time)]) for n in range(0, len(Paleolat.values.tolist()), len(time))])
+
+
+# df = pd.DataFrame(vals) 
+# df.columns = ['Age', 'Paleolat', 'Dec','Inc','Pole_lat','Pole_Long'] 
+# Paleolat = df['Paleolat']
+
+# pl_df = pd.DataFrame([(Paleolat.values.tolist()[n:n+len(time)]) for n in range(0, len(Paleolat.values.tolist()), len(time))])
 
 #RUN THIS IF YOU'RE MAKING FIG. 3
-# pl_temp = np.repeat(Read.site_lat.tolist(),len(time))
+# pl_temp = np.repeat(90,len(time))
 # pl_df = pd.DataFrame([(pl_temp[n:n+101]) for n in range(0, len(pl_temp), len(time))])

src/User_Interface.py
@@ -17,7 +17,7 @@ import numpy as np
             2 = 3He in clinopyroxene
             3 = 3He in olivine
             4 = 21Ne in quartz
-        system_b : integer 0 - 4 (specify clinopyroxene)
+        system_b : integer 0 - 4 (specify cpx/opx)
             0 = N/A (non-cpx)
             1 = enstatite
             2 = ferrosilite
@@ -56,12 +56,12 @@ import numpy as np
 system = 2
 system_b = 4
 system_c = 0
-stdatm = 0
+stdatm = 1 
 muons = False
 timerange = [0,25+0.05]
 plate = 'SA'
 
-#The authors do not recommend changing the resolution below #
+#The authors do not recommend changing the resolution below 
 resolution = int(250000)/10**6
 
 time = np.arange(timerange[0], timerange[1], resolution) # time bin#for 250ka resolution, with first bin being 50 ka

src/atm_depth.py
@@ -20,7 +20,21 @@ import scipy as sci
 import Read
 import Pmag_paleolat
 import User_Interface
+import math
 
+def truncate(number, decimals=0):
+    """
+    Returns a value truncated to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer.")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more.")
+    elif decimals == 0:
+        return math.trunc(number)
+
+    factor = 10.0 ** decimals
+    return math.trunc(number * factor) / factor
 time = User_Interface.time 
 
 lon_repeated = np.repeat(Read.lon,len(time))
@@ -77,15 +91,23 @@ if User_Interface.stdatm == 0: #ERA40
 
 empty = []
 if User_Interface.stdatm == 1:
-    for i in range(len(differential)):
-        sp = 1013.25 * np.exp((gmr/differential[i])*(np.log(288.15) - np.log(288.15 - (alt_list[i]*differential[i]))))
+    for i in range(len(alt_list)):
+        differential = 0.0065
+        sp = 1013.25 * np.exp((gmr/differential)*(np.log(288.15) - np.log(288.15 - (alt_list[i]*differential))))
         empty.append(sp)
     sample_pressure = pd.DataFrame([(empty[n:n+len(time)]) for n in range(0, len(empty), len(time))])
 
+
+x_updated = []
+for i in range(len(empty)):
+    temp = truncate(empty[i],0)
+    x_updated.append(float(temp))
+xdf = pd.DataFrame([(x_updated[n:n+len(time)]) for n in range(0, len(x_updated), len(time))])
+
 #convert pressure to atmospheric depth
 def atmdepth(x):
-    return x*1.019716
-x = atmdepth(sample_pressure)
+    return x*(1.019716)
+x = atmdepth(xdf)
 
-# xn = np.repeat(1033, 95)
+# xn = np.repeat(1013.25*1.019716, 2020)
 # x = pd.DataFrame([(xn[n:n+len(time)]) for n in range(0, len(xn), len(time))])

src/expage.py
@@ -29,14 +29,34 @@ erosion = np.repeat(0,20)
 dt = 250000
 Pmu = 0.23 #larsen et al
 
-# dt = np.repeat(250000,len(User_Interface.time)-1)
-# dt = np.insert(dt,0, 50000)
+import math
 
+# def truncate(number, decimals=0):
+#     """
+#     Returns a value truncated to a specific number of decimal places.
+#     """
+#     if not isinstance(decimals, int):
+#         raise TypeError("decimal places must be an integer.")
+#     elif decimals < 0:
+#         raise ValueError("decimal places has to be 0 or more.")
+#     elif decimals == 0:
+#         return math.trunc(number)
+
+#     factor = 10.0 ** decimals
+#     return math.trunc(number * factor) / factor
+
+# sthick_updated = []
+# for i in range(len(sthick)):
+#     temp = truncate(sthick[i],2)
+#     sthick_updated.append(temp)
+    
 #if User_Interface.system == 4: 
 #spallation part
+
 # def func(sthick,tempvals):
 #     return sthick*slhl*tempvals
 
+    
 # for i in range(len(scaling_factor.Siteprod_df)): #how many samples 
 #     for j in range(len(scaling_factor.Siteprod_df.iloc[0])): #time length
 #         temp = (scaling_factor.Siteprod_df.iloc[i][j]* dt)
@@ -58,8 +78,8 @@ Pmu = 0.23 #larsen et al
 
 # iteration = []
 # for i in range (len(tempvals_df)):
-#     x = tempvals_df[0][i]
-#     y = tempvals_df_mu[0][i]
+#     x = tempvals_df.iloc[i][0]
+#     y = tempvals_df_mu.iloc[i][0]
 #     for j in range(len(tempvals_df.iloc[0])):
 #         l = func(sthick[i],x) + funcmu(y)
 #         if l < n0[i]:
@@ -71,18 +91,16 @@ Pmu = 0.23 #larsen et al
         
 # exp_age = []
 # for i in range (len(tempvals_df)):
-#     for j in range(len(tempvals_df.iloc[0])):
-#         dt2 = (n0[i] - (sthick[i] * slhl * (np.sum(tempvals_df.iloc[i][0:iteration[i]])) + (Pmu*(np.sum(tempvals_df_mu.iloc[i][0:iteration[i]]))))) / (sthick[i] * slhl * (tempvals_df.iloc[i][iteration[i]+1]/dt)) + (Pmu * (tempvals_df_mu.iloc[i][iteration[i]+1]/dt))  
-#         expage = iteration[i] * dt + dt2
+#     a = (np.sum(tempvals_df.iloc[i][0:iteration[i]]))
+#     b = (np.sum(tempvals_df_mu.iloc[i][0:iteration[i]]))
+#     dt2 = ((n0[i] - (sthick[i] * slhl * a) - (Pmu * b))/ (stexphick[i] * slhl) * Pmu)/ scaling_factor.Siteprod_df.iloc[i][iteration[i]+1]
+#     expage = iteration[i] * dt + dt2
 #     exp_age.append(expage)
 
-
-
-
- 
 def func(sthick,tempvals):
     return sthick*slhl*tempvals
 
+    
 for i in range(len(scaling_factor.Siteprod_df)): #how many samples 
     for j in range(len(scaling_factor.Siteprod_df.iloc[0])): #time length
         temp = (scaling_factor.Siteprod_df.iloc[i][j]* dt)
@@ -93,8 +111,8 @@ for i in range(len(scaling_factor.Siteprod_df)): #how many samples
 tempvals_df = pd.DataFrame([(tempvals[n:n+len(User_Interface.time)]) for n in range(0, len(tempvals), len(User_Interface.time))])
 
 iteration = []
-for i in range (len(tempvals_df)):
-    x = tempvals_df[0][i]
+for i in range(len(tempvals_df)):
+    x = tempvals_df.iloc[i][0]
     for j in range(len(tempvals_df.iloc[0])):
         l = func(sthick[i],x)
         if l < n0[i]:
@@ -102,10 +120,11 @@ for i in range (len(tempvals_df)):
         else:
             iteration.append(j)
             break
+            
         
 exp_age = []
 for i in range (len(tempvals_df)):
-    for j in range(len(tempvals_df.iloc[0])):
-        dt2 = (n0[i] - sthick[i] * slhl * (np.sum(tempvals_df.iloc[i][0:iteration[i]]))) / (sthick[i] * slhl * (tempvals_df.iloc[i][iteration[i]+1]/dt)) 
-        expage = iteration[i] * dt + dt2
+    a = (np.sum(tempvals_df.iloc[i][0:iteration[i]]))
+    dt2 = ((n0[i]/(sthick[i] * slhl)) - a) / (scaling_factor.Siteprod_df.iloc[i][iteration[i]])
+    expage = iteration[i] * dt + dt2
     exp_age.append(expage)

src/neutron_spallation.py
@@ -33,7 +33,7 @@ Et = 2.5e-8; #Thermal neutron energy in MeV
 
 smin = 400; #Units of MV
 smax = 1200; #Units of MV
-w = 0.06 #water content. 
+w = 0.2 #water content. 
 
 # Ground-Level Spectrum
 g3 = 10**(Read.ground_level_spectrum.iloc[0]['values'] + (Read.ground_level_spectrum.iloc[1]['values'])/(w + Read.ground_level_spectrum.iloc[2]['values']))
@@ -169,51 +169,55 @@ for n in range(len(c4_df)):
 PhiGMev_temp = pd.DataFrame([(df2[n:n+200]) for n in range(0, len(df2), len(E))])  
 PhiGMev = PhiGMev_temp.T
 
-
 for i in range(len(Rc.Rc)*len(time)):
     
     if User_Interface.system == 1: #qtz
-        Natoms = 2.00456e22
-        p3n_temp_qtz = (np.trapz(PhiGMev.iloc[:,i]*Read.Onx3df[0],E_df.iloc[0,:])+ np.trapz(PhiGMev.iloc[:,i]*(Read.Sinx3df[0]/2), E_df.iloc[0,:]))*(Natoms*1e-27*3.1536e7)
+        NatomsO = Read.NatomsQtzO
+        NatomsSi = Read.NatomsQtzSi
+        p3n_temp_qtz = (np.trapz(PhiGMev.iloc[:,i]*Read.Onx3df[0],E_df.iloc[0,:])
+        + np.trapz(PhiGMev.iloc[:,i]*(Read.Sinx3df[0]/2),E_df.iloc[0,:]))*2.00600000000000e+22*1e-27*3.1536e7
         p3n_qtz.append(p3n_temp_qtz)
 
     #Inserted from Dave Parmelee's code (MS thesis, NMT 2014) to account for composition
     #dependence of clinopyroxene
     
     if User_Interface.system == 2: #cpx
-        if User_Interface.system_b == 1:
-            Natoms = 1.79965e22
-        if User_Interface.system_b == 2:
-            Natoms = 1.36941e22
-        if User_Interface.system_b == 3:
-            Natoms = 1.55528e22
-        if User_Interface.system_b == 4:
-            Natoms= 2 * 1.53124e22
         p3ndf_cpx = (np.trapz(PhiGMev.iloc[:,i]*Read.Onx3df[0], E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Sinx3df[0]*(1.92/6)),E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Alnx3df[0]*(0.12/6)),E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Mgnx3df[0]*(0.67/6)), E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Fenx3df[0]*(0.31/6)), E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Canx3df[0]*(0.86/6)), E_df.iloc[0,:]))*(Natoms*1e-27*3.1536e7)
+        np.trapz(PhiGMev.iloc[:,i]*Read.Sinx3df[0]*1.92/6,E_df.iloc[0,:]) +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Alnx3df[0]*0.12/6,E_df.iloc[0,:]) +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Mgnx3df[0]*0.67/6, E_df.iloc[0,:]) +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Fenx3df[0]*0.31/6, E_df.iloc[0,:]) +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Canx3df[0]*0.86/6, E_df.iloc[0,:]))*2.00600000000000*10**22*1*10**-27*3.1536*10**7
         p3n_cpx.append(p3ndf_cpx)
 
+    
     if User_Interface.system == 3: #olivine
-        if User_Interface.system_c == 1:
-            Natoms = 1.71214e22
-        if User_Interface.system_c == 2:
-            Natoms = 1.1821e22
-        if User_Interface.system_c == 3:
-            Natoms = 1.57124e22
-        p3ndf_ol = (np.trapz(PhiGMev.iloc[:,i]*Read.Onx3df[0], E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Sinx3df[0]*(1/4)), E_df.iloc[0,:]) + 
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Mgnx3df[0]*(1.1/4)), E_df.iloc[0,:]) +
-        np.trapz(PhiGMev.iloc[:,i]*(Read.Fenx3df[0]*(0.9/4)), E_df.iloc[0,:]))*(Natoms*1e-27*3.1536e7)
+        if User_Interface.system_c == 1: #Forsterite
+            NatomsO = Read.NatomsOlFoO
+            NatomsSi = Read.NatomsOlFoSi
+            NatomsMg = Read.NatomsOlFoMg
+            NatomsFe = 0
+        if User_Interface.system_c == 2: #Fayalite
+            NatomsO = Read.NatomsOlFaO
+            NatomsSi = Read.NatomsOlFaSi
+            NatomsMg = 0
+            NatomsFe = Read.NatomsOlFaFe
+        if User_Interface.system_c == 3: #F8
+            NatomsO = Read.NatomsOlFo80O
+            NatomsSi = Read.NatomsOlFo80Si
+            NatomsMg = Read.NatomsOlFo80Mg
+            NatomsFe = Read.NatomsOlFo80Fe
+
+        p3ndf_ol = (np.trapz(PhiGMev.iloc[:,i]*Read.Onx3df[0], E_df.iloc[0,:])*NatomsO +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Sinx3df[0], E_df.iloc[0,:])*NatomsSi + 
+        np.trapz(PhiGMev.iloc[:,i]*Read.Mgnx3df[0], E_df.iloc[0,:])*NatomsMg +
+        np.trapz(PhiGMev.iloc[:,i]*Read.Fenx3df[0], E_df.iloc[0,:])*NatomsFe)*1e-27*3.1536e7
         p3n_ol.append(p3ndf_ol)
 
     #21-Ne
     if User_Interface.system == 4:
         Natoms = 1.00228e22
-        p21ndf_qtz = (np.trapz(PhiGMev.iloc[:,i]*Read.Sinx21df[0],E_df.iloc[0,:])) *(Natoms*1e-27*3.1536e7)
+        p21ndf_qtz = (np.trapz(PhiGMev.iloc[:,i]*Read.Sinx21df[0],E_df.iloc[0,:])) *(NatomsSi*1e-27*3.1536e7)
         p21n_qtz.append(p21ndf_qtz)
 
 

src/proton_spallation.py
@@ -28,7 +28,7 @@ time = User_Interface.time
 s = 624.5718; #Solar modulation- uses constant value that Lifton (2008)/code uses for samples beyond 10 Ma
 smin = 400; #Units of MV
 smax = 1200; #Units of MV
-w = 0.06
+w = 0.2
 
 #PROTONS
 p3p_cpx = []
@@ -184,41 +184,87 @@ p3p_ol = []
 p21p_qtz = []
 for i in range(len(Rc.Rc)*len(time)):
     if User_Interface.system == 1: #qtz
-        p3p_temp_qtz = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0],E) + np.trapz(phiPtot.T.iloc[:,i]*(Read.Sipx3df[0]/2),E))*2.00600000000000e22*1e-27*3.1536e7   
+        NatomsO = Read.NatomsQtzO
+        NatomsSi = Read.NatomsQtzSi
+        p3p_temp_qtz = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0],E_df.iloc[0,:]) + np.trapz(phiPtot.T.iloc[:,i]*(Read.Sipx3df[0]/2),E_df.iloc[0,:]))*2.00600000000000e+22*1e-27*3.1536e7  
         p3p_qtz.append(p3p_temp_qtz)
-        
+
     if User_Interface.system == 2:
-        if User_Interface.system_b == 1:
-            Natoms = 1.79965e22
-        if User_Interface.system_b == 2:
-            Natoms = 1.36941e22
-        if User_Interface.system_b == 3:
-            Natoms = 1.55528e22
-        if User_Interface.system_b == 4:
-            Natoms = 2 * 1.53124e22
         p3p_temp_cpx = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0], E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Sipx3df[0]*1.92/6),E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Alpx3df[0]*0.12/6),E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Mgpx3df[0]*0.67/6), E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Fepx3df[0]*0.31/6), E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Capx3df[0]*0.86/6), E))*(Natoms*1e-27*3.1536e7)
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx3df[0]*1.92/6,E) +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Alpx3df[0]*0.12/6,E) +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Mgpx3df[0]*0.67/6, E) +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Fepx3df[0]*0.31/6, E) +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Capx3df[0]*0.86/6, E))*2.00600000000000*10**22*1*10**-27*3.1536*10**7
         p3p_cpx.append(p3p_temp_cpx)
+        # if User_Interface.system_b == 1: #Enstatite
+        #     NatomsO = Read.NatomsOPxEnO
+        #     NatomsSi = Read.NatomsOPxEnSi
+        #     NatomsAl = 0
+        #     NatomsMg = Read.NatomsOPxEnMg
+        #     NatomsFe = 0
+        #     NatomsCa = 0
+        # if User_Interface.system_b == 2: #Ferrosilite
+        #     NatomsO = Read.NatomsOPxFsO
+        #     NatomsSi = Read.NatomsOPxFsSi
+        #     NatomsAl = 0
+        #     NatomsMg = 0
+        #     NatomsFe = Read.NatomsOPxFsFe
+        #     NatomsCa = 0
+        # if User_Interface.system_b == 3: #Wollastonite
+        #     NatomsO = Read.NatomsCPxWoO
+        #     NatomsSi = Read.NatomsCPxWoSi
+        #     NatomsAl = 0
+        #     NatomsMg = 0
+        #     NatomsFe = 0
+        #     NatomsCa = Read.NatomsCPxWoCa  
+        # if User_Interface.system_b == 4: #Augite
+        #     NatomsO = Read.NatomsCPxAuO
+        #     NatomsSi = Read.NatomsCPxAuSi 
+        #     NatomsAl = Read.NatomsCPxAuAl 
+        #     NatomsMg = Read.NatomsCPxAuMg 
+        #     NatomsFe = Read.NatomsCPxAuFe
+        #     NatomsCa = Read.NatomsCPxAuCa 
+        # p3p_temp_cpx = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0], E_df.iloc[0,:])*NatomsO +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx3df[0],E_df.iloc[0,:])*NatomsSi +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Alpx3df[0],E_df.iloc[0,:])*NatomsAl +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Mgpx3df[0], E_df.iloc[0,:])*NatomsMg +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Fepx3df[0], E_df.iloc[0,:])*NatomsFe +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Capx3df[0], E_df.iloc[0,:])*NatomsCa)*(1e-27*3.1536e7)
+        # p3p_cpx.append(p3p_temp_cpx)
+        
+        # p3p_temp_cpx = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0], E_df.iloc[0,:]) +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx3df[0],E_df.iloc[0,:]*1.92/6) +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Alpx3df[0],E_df.iloc[0,:]*0.12/6) +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Mgpx3df[0], E_df.iloc[0,:]*0.67/6) +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Fepx3df[0], E_df.iloc[0,:]*0.31/6) +
+        # np.trapz(phiPtot.T.iloc[:,i]*Read.Capx3df[0], E_df.iloc[0,:]*0.86/6))*1.5312e22*(1e-27*3.1536e7)
+        # p3p_cpx.append(p3p_temp_cpx)
     if User_Interface.system == 3: 
-        if User_Interface.system_c == 1:
-            Natoms = 1.71214e22
-        if User_Interface.system_c == 2:
-            Natoms = 1.1821e22
-        if User_Interface.system_c == 3:
-            Natoms = 1.57124e22
-        p3p_temp_ol = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0], E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Sipx3df[0]*1/4), E) + 
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Mgpx3df[0]*1.1/4), E) +
-        np.trapz(phiPtot.T.iloc[:,i]*(Read.Fepx3df[0]*0.9/4), E))*(Natoms*1e-27*3.1536e7)
+        if User_Interface.system_c == 1: #Forsterite
+            NatomsO = Read.NatomsOlFoO
+            NatomsSi = Read.NatomsOlFoSi
+            NatomsMg = Read.NatomsOlFoMg
+            NatomsFe = 0
+        if User_Interface.system_c == 2: #Fayalite
+            NatomsO = Read.NatomsOlFaO
+            NatomsSi = Read.NatomsOlFaSi
+            NatomsMg = 0
+            NatomsFe = Read.NatomsOlFaFe
+        if User_Interface.system_c == 3: #F8
+            NatomsO = Read.NatomsOlFo80O
+            NatomsSi = Read.NatomsOlFo80Si
+            NatomsMg = Read.NatomsOlFo80Mg
+            NatomsFe = Read.NatomsOlFo80Fe
+        p3p_temp_ol = (np.trapz(phiPtot.T.iloc[:,i]*Read.Opx3df[0], E_df.iloc[0,:])*NatomsO +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx3df[0], E_df.iloc[0,:])*NatomsSi + 
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Mgpx3df[0], E_df.iloc[0,:])*NatomsMg +
+        np.trapz(phiPtot.T.iloc[:,i]*Read.Fepx3df[0], E_df.iloc[0,:])*NatomsFe)*1e-27*3.1536e7        
         p3p_ol.append(p3p_temp_ol)
     #21-Ne
     if User_Interface.system == 4:
         Natoms = 1.00228e22
-        p21p_temp_qtz = (np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx21df[0],E)) *(Natoms*1e-27*3.1536e7)
+        p21p_temp_qtz = (np.trapz(phiPtot.T.iloc[:,i]*Read.Sipx21df[0],E)) *(NatomsSi*1e-27*3.1536e7)
         p21p_qtz.append(p21p_temp_qtz)
 
 