637af9903044663098021e26d467be7405bc4ff3
mmijjum <mmijjum@purdue.edu>
2024-04-18 11:25:33 -0400

    cleaned up atm_depth.py

--- src/atm_depth.py
+++ src/atm_depth.py
@@ -48,7 +48,7 @@ for j in range(len(delta)):
         alt_list[i+1]=alt_list[i]+delta[j]
      
  
-if Read.stdatm == 0: #ERA40
+if Read.stdatm == 0: #ERA40, using dataset from LSDn
 
     lat_numpy = Read.ERA40lat.to_numpy()
     lon_numpy = Read.ERA40lon.to_numpy()
@@ -96,7 +96,9 @@ if Read.stdatm == 0: #ERA40
         empty.append(sp)
     sample_pressure = pd.DataFrame([(empty[n:n+len(time)]) for n in range(0, len(empty), len(time))])
 
-if Read.stdatm == 1:
+if Read.stdatm == 1: #Standard atmosphere
+    gmr = -0.03417; # Assorted constants
+    dtdz = 0.0065; # Lapse rate from standard atmosphere
         #Lifton Lapse Rate Fit to COSPAR CIRA-86 <10 km altitude
     lr = [-6.1517E-03, -3.1831E-06, -1.5014E-07, 1.8097E-09, 1.1791E-10, -6.5359E-14, -9.5209E-15]
     empty = []
@@ -108,10 +110,10 @@ if Read.stdatm == 1:
     sample_pressure = pd.DataFrame([(empty[n:n+len(time)]) for n in range(0, len(empty), len(time))])
 
 if Read.stdatm == 2: #climate simulation
-    lat_numpy = Read.climate_lat.to_numpy()
+    lat_numpy = Read.climate_lat.to_numpy() #read in lat/lon grids from climate simulation (slightly different from ERA40)
     lon_numpy = Read.climate_lon.to_numpy()
    
-    a = Read.climate_mslp_a
+    a = Read.climate_mslp_a #mean sea level pressures for each time slice, each letter denotes new time slice
     b = Read.climate_mslp_b
     c = Read.climate_mslp_c
     d = Read.climate_mslp_d
@@ -127,9 +129,9 @@ if Read.stdatm == 2: #climate simulation
     n = Read.climate_mslp_n
     o = Read.climate_mslp_o
     
-    mslp_merged = (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o)
+    mslp_merged = (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o) #merge all MSLP time slices into a single grid
     
-    at = Read.climate_mat_a
+    at = Read.climate_mat_a #repeat above, but for mean annual temp
     bt = Read.climate_mat_b
     ct = Read.climate_mat_c
     dt = Read.climate_mat_d
@@ -165,25 +167,25 @@ if Read.stdatm == 2: #climate simulation
     sample_pressure = []
     
     time = Read.time
-    domain = [0,2,7.25,12.75,17.25,22.75,28.50,33.50,37.75,42,48.50,54,58.25,63.50,67.50] #Ma of each bin
-    mini_durations = [0,8,21,22,18,22,23,20] #how many MCADAM bins in each domain bin
-    start = np.argmax(domain <= time[0])
-    stop = (np.argmax(domain > time[-1])) - 1
-    if stop == start:
+    domain = [0,2,7.25,12.75,17.25,22.75,28.50,33.50,37.75,42,48.50,54,58.25,63.50,67.50,72] #using midpoints, the range of each time slice (in Ma)
+    mini_durations = [0,8,21,22,18,22,23,20,17,17,26,22,17,21,16,18] #how many MCADAM bins in climate bin (e.g., between 0-2 Ma, there are 8 MCADAM bins (sized 250 ka))
+    stop = (np.argmax(domain > time[-1])) - 1 #determine where in the domain to start, given user input of start time
+    start = np.argmax(domain <= time[0])  #determine where in the domain to end, given user input of end time
+    if stop == start: #determine how many consecutive bins will be used. If stop == start, only one bin is used.
         duration = range(start)
     
     else:
         duration = range(start,stop+1)
     
-    for j in range(len(Read.site_lon)):
+    for j in range(len(Read.site_lon)): #interpolate to identify temp/MSLP for each time bin, and resize to match 
         x = mini_durations[0]
-        for k in range(len(duration)):
+        for k in duration:
             t = sci.interpolate.interp2d(lon_numpy,lat_numpy,mat_merged[k])
             p = sci.interpolate.interp2d(lon_numpy,lat_numpy,mslp_merged[k])
             y = x + mini_durations[k+1]
             site_T = t(lon_df.loc[j][x:y],Pmag_paleolat.pl_df.loc[j][x:y])
             site_T_degK = site_T + 273.15
-            temp.append(site_T)
+            temp.append(site_T_degK)
             site_slp = p(lon_df.loc[j][x:y],Pmag_paleolat.pl_df.loc[j][x:y])
             slp.append(site_slp)
             x = y
@@ -216,11 +218,12 @@ if Read.stdatm == 2: #climate simulation
     
     for i in range(len(pressures)):
         sp = pressures[i] * np.exp( (gmr/differential[i]) * (np.log(temperatures[i]) - np.log(temperatures[i] - (alt_list[i]*differential[i])) ) )
-        sp = float(sp)
+        #sp = float(sp)
         empty.append(sp)
 
 sample_pressure = pd.DataFrame([(empty[n:n+len(time)]) for n in range(0, len(empty), len(time))])
-    
+sample_pressure = sample_pressure.fillna(0)
+
 #convert pressure to atmospheric depth
 def atmdepth(x):
     return x*(1.019716)
